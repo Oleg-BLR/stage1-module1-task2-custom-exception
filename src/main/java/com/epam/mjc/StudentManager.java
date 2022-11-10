b37cf57691326365ff2b5b98d2886d8b59b5b0af
Oleg <Oleg-BLR@yandex.ru>
2022-11-10 20:23:46 +0300

    Solution 01

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -3,11 +3,17 @@ package com.epam.mjc;
 
 public class StudentManager {
 
-    private static final long[] IDs = {1, 2, 3, 4, 555, 6, 7, 8, 9, 10, 11};
+    private static final long[] IDs = {1, 22, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 //    private static final long[] IDs = {1, 2, 11, 3};
 
-    public Student find(long studentID) {    //throws CouldNotFindStudentWithIDException { - с этим тоже не работает
-        return Student.getValueOf(studentID);
+    public Student find(long studentID) {
+        Student student = Student.getValueOf(studentID);
+
+        if (student != null) {
+            return student;
+        } else {
+            throw new CouldNotFindStudentWithIDException("Could not find student with ID " + studentID);
+        }
     }
 
 
@@ -18,13 +24,12 @@ public class StudentManager {
             Student student = manager.find(IDs[i]);
             try {
                 System.out.println("Student name " + student.getName());
-            } catch (NullPointerException e) {    /* здесь с моим CouldNotFindStudentWithIDException не работает,
-                                                даже если CouldNotFindStudentWithIDException extends NullPointerException.
-                                                Почему?
-                                                */
+            } catch (NullPointerException e) {
                 System.out.println("Could not find student with ID " + IDs[i]);
             }
         }
     }
-
 }
+
+
+
